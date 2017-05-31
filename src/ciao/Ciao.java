2888907316d5a80ea59756b7c5cfa1d7dcb5bd21
DaniMain <daniele.mianella@gmail.com>
2017-05-31 12:04:39 +0200

    ciao

--- src/ciao/Ciao.java
+++ src/ciao/Ciao.java
@@ -3,6 +3,6 @@ package ciao;
 public class Ciao {
 	public static void main(String[] args) {
 		System.out.println("bella patti");
-		System.out.println("sono DaniMain");
+		System.out.println("avevo messo sul master");
 	}
 }
