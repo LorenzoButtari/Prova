c35970e4f8d924f849c547b704cde0863018d885
DaniMain <daniele.mianella@gmail.com>
2017-05-31 12:09:20 +0200

    prova DaniMain

--- src/ciao/Ciao.java
+++ src/ciao/Ciao.java
@@ -4,5 +4,6 @@ public class Ciao {
 	public static void main(String[] args) {
 		System.out.println("bella patti");
 		System.out.println("avevo messo sul master");
+		System.out.println("ora sono DaniMain (forse)");
 	}
 }
